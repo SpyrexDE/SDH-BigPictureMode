c7c3db5773f3059a4f7bcc273c2b554c76538a09
Spyrex <malikfk100@gmail.com>
2022-05-06 17:19:06 +0200

    Use SteamClient.System instead of commandline

--- main.py
+++ main.py
@@ -3,10 +3,6 @@ import subprocess
 class Plugin:
     BPM = False
 
-    async def open_BPM(self):
-        self.BPM = True
-        subprocess.Popen("steam steam://open/bigpicture", shell=True)
-
     async def close_BPM(self):
         self.BPM = False
         subprocess.Popen("steam -shutdown", shell=True)
