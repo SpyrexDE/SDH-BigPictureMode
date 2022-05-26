01233f14435556ecfe12a6f483136367ab83b28b
SpyrexDE <malikfk100@gmail.com>
2022-05-26 11:17:25 +0200

    Change method to close BP mode

--- main.py
+++ main.py
@@ -5,7 +5,10 @@ class Plugin:
 
     async def close_BPM(self):
         self.BPM = False
-        subprocess.Popen("steam -shutdown", shell=True)
+        subprocess.Popen("pkill -sigint -x steam", shell=True)
     
+    async def set_BPM(self, val):
+        self.BPM = val
+
     async def get_BPM(self):
         return self.BPM
