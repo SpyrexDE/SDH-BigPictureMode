096a3baa7316b0e526a8ffe1c16e3126496a2fae
Iris Nebula <6500579+milroneth@users.noreply.github.com>
2022-05-26 15:47:24 +0300

    Update main.py

--- main.py
+++ main.py
@@ -7,8 +7,8 @@ class Plugin:
         self.BPM = False
         subprocess.Popen("pkill -sigint -x steam", shell=True)
     
-    async def set_BPM(self, val):
-        self.BPM = val
+    async def set_BPM(self):
+        self.BPM = True
 
     async def get_BPM(self):
         return self.BPM
